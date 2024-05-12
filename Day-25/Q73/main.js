1fd77e5a49bef0ee1a6de86dd1e697f38ef6e1f2
AZEEMSARDAR <AZEEMSARDAR228@GMAIL.COM>
2024-05-12 22:49:11 +0500

    add day 25-26

---
+++ Day-25/Q73/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// This function shows how to assign and update variable values
+function updateVariable() {
+    let number = 10; // Initially assigns the value 10
+    console.log("Initial value:", number); // Logs the initial value
+    number = 30; // Update the value to 30
+    console.log("Updated value:", number); // Logs the updated value
+}
+updateVariable();
+// We assign a value to a variable and then update it, showing how values can change.
